27fb1dbcefd3b893fe31da75b5247a202b9726a3
Youssef Bazzi <yoyobazzi3@gmail.com>
2024-07-28 16:53:59 -0700

    # This is a combination of 4 commits. # This is the 1st commit message:
    test linter with bad py
    
    # This is the commit message #2:
    
    Update main.yml
    # This is the commit message #3:
    
    Update __init__.py
    # This is the commit message #4:
    
    Delete .github/workflows/requirements.txt

--- factories/__init__.py
+++ factories/__init__.py
@@ -1,1 +1,9 @@
+"""
+This module initializes the factories package.
+
+The factories package contains factory classes and methods
+used for creating instances of different classes and managing
+object creation in the project.
+"""
+
 from .use_cases import list_product_use_case, find_product_by_id_use_case
